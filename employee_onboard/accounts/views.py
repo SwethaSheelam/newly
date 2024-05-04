982d9515f83b9fa6935975629e7de9b35d8e41de
sheelamswetha <sheelamswetha5@gmail.com>
2024-05-04 23:13:03 +0530

    updating

---
+++ employee_onboard/accounts/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import UserRegistrationSerializer
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['first_name'] = user.first_name
+        # ...
+
+        return token
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class=MyTokenObtainPairSerializer
+
+
+
+
+# Create your views here.
+@api_view()
+def getRoutes(request):
+    routes=[
+        '/token',
+        '/token/referesh',
+    ]
+    return Response(routes)
+
+
+# Registration logic
+@api_view(['POST'])
+def register_user(request):
+    if request.method == 'POST':
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
